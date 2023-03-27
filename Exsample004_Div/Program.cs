610402c514b69d71762cbf75173bf817fd5ca3c3
Galina Pochtova <lina.pa1@mail.ru>
2023-03-27 14:08:30 +0500

    Random

--- Exsample004_Div/Program.cs
+++ Exsample004_Div/Program.cs
@@ -1,4 +1,6 @@
 ﻿
-double numberA = 12;
-double numberB = 5;
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); // 1 2 3 4 ....9
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
