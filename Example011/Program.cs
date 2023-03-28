d8837bcb94cb1c2fe14e783b111803ff1276fad9
Galina Pochtova <lina.pa1@mail.ru>
2023-03-28 12:34:48 +0500

    Exsample011

---
+++ Example011/Program.cs
@@ -0,0 +1,22 @@
+﻿// new int [10] - сщздай новый массив, в которм 10 элементов (ни чего не поняла в это коде)
+/*void FillArray(int[ collection])
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+int[] array = new int [10];
+*/
+void FillArray(int[ collection])
+ 
+ void PrintArray(int[] col)
+
+ int[] array = new int[10];
+
+ FillArray(array);
+ PrintArray(array);
+
